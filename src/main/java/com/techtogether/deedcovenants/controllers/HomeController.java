fc3b38457132435284e0bda399d5865b2ffc1c63
krystalwhite <krystalsnowhite@gmail.com>
2022-01-29 11:02:32 -0600

    navbar works, cleaned up code for main

--- src/main/java/com/techtogether/deedcovenants/controllers/HomeController.java
+++ src/main/java/com/techtogether/deedcovenants/controllers/HomeController.java
@@ -1,31 +1,14 @@
 package com.techtogether.deedcovenants.controllers;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
-//    @GetMapping()
-//    public String renderIndex() {
-//        return "index";
-//    }
-
-    @Value("${placesApiKey}")
-    private String placesApiKey;
-
     @GetMapping()
-    public String renderSearch(Model model) {
-        model.addAttribute("placesApiKey", placesApiKey);
+    public String renderIndex() {
         return "index";
     }
 
-    @PostMapping()
-    public String processSearchForm(Model model) {
-        model.addAttribute("placesApiKey", placesApiKey);
-        return "redirect:";
-    }
 }
--- src/main/java/com/techtogether/deedcovenants/controllers/SearchController.java
+++ src/main/java/com/techtogether/deedcovenants/controllers/SearchController.java
@@ -1,10 +1,12 @@
 package com.techtogether.deedcovenants.controllers;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class SearchController {
 
     @Value("${placesApiKey}")
