3ae36927678fff857bab33b99e8099e5f325cd26
millylane003 <emilykgerst19@gmail.com>
2022-01-28 19:27:10 -0600

    first commit

---
+++ src/main/java/com/techtogether/deedcovenants/DeedCovenantsApplication.java
@@ -0,0 +1,13 @@
+package com.techtogether.deedcovenants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeedCovenantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeedCovenantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/techtogether/deedcovenants/DeedCovenantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtogether.deedcovenants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeedCovenantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
