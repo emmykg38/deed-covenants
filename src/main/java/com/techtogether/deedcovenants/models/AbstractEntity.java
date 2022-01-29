d8e448a56157c50125eb84d7129aaeee7362bba2
millylane003 <emilykgerst19@gmail.com>
2022-01-28 20:19:56 -0600

    added abstract entity

---
+++ src/main/java/com/techtogether/deedcovenants/models/AbstractEntity.java
@@ -0,0 +1,15 @@
+package com.techtogether.deedcovenants.models;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class AbstractEntity {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    public int getId() { return id; }
+}
