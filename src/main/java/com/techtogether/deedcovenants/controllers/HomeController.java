f7126c2fcc11be87f96302611530d1e19eb993cc
krystalwhite <krystalsnowhite@gmail.com>
2022-01-29 08:39:04 -0600

    added java client to build.gradle

--- src/main/java/com/techtogether/deedcovenants/controllers/HomeController.java
+++ src/main/java/com/techtogether/deedcovenants/controllers/HomeController.java
@@ -1,13 +1,30 @@
 package com.techtogether.deedcovenants.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
 
+//    @GetMapping()
+//    public String renderIndex() {
+//        return "index";
+//    }
+
+    @Value("${placesApiKey}")
+    private String placesApiKey;
+
     @GetMapping()
-    public String renderIndex() {
+    public String renderSearch(Model model) {
+        model.addAttribute("placesApiKey", placesApiKey);
         return "index";
+    }
+
+    @PostMapping()
+    public String processSearchForm(Model model) {
+        return "redirect:";
     }
 }
