531bd9c1b0c9d8afcce809ddf9aa5afea0120bcf
krystalwhite <krystalsnowhite@gmail.com>
2022-01-29 14:13:27 -0600

    thinking about searching the database

--- src/main/java/com/techtogether/deedcovenants/controllers/SearchController.java
+++ src/main/java/com/techtogether/deedcovenants/controllers/SearchController.java
@@ -1,16 +1,23 @@
 package com.techtogether.deedcovenants.controllers;
 
+import com.techtogether.deedcovenants.data.DeedRepository;
+import com.techtogether.deedcovenants.models.Deed;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SearchController {
 
     @Value("${placesApiKey}")
     private String placesApiKey;
+
+    @Autowired
+    private DeedRepository deedRepository;
 
     @GetMapping("/search")
     public String renderSearch(Model model) {
@@ -19,12 +26,20 @@ public class SearchController {
     }
 
     @PostMapping("/search")
-    public String processSearchForm(Model model) {
-        return "redirect:";
-    }
+    public String processSearchForm(Model model, @RequestParam String searchTerm) {
+        Boolean hasCovenant;
+        Iterable<Deed> listDeeds = deedRepository.findAll();
+//        do something with Iterable and Optional???
+        if (listDeeds.equals(searchTerm)) {
+            hasCovenant = true;
+        }
+//        model.addAttribute("listDeeds", listDeeds);
 
-    @GetMapping("/response")
-    public String renderResponse() {
         return "response";
     }
+
+//    @GetMapping("/response")
+//    public String renderResponse(Model model) {
+//        return "response";
+//    }
 }
