9d74568fdb0824dfc86ab379402577d29e2ae6ea
millylane003 <emilykgerst19@gmail.com>
2022-01-29 13:11:41 -0600

    added postmapping controller method for add deed

--- src/main/java/com/techtogether/deedcovenants/controllers/AddDeedController.java
+++ src/main/java/com/techtogether/deedcovenants/controllers/AddDeedController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -23,5 +25,11 @@ public class AddDeedController {
         model.addAttribute(new Deed());
         model.addAttribute("placesApiKey", placesApiKey);
         return "add";
+    }
+
+    @PostMapping("/add")
+    public String processAddDeedForm(@ModelAttribute Deed deed, Model model) {
+        deedRepository.save(deed);
+        return "redirect:";
     }
 }
